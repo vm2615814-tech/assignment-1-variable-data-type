977bc5e3553748208a35c92eeed0072950e1e3a4
VIVEK KUMAR MISHRA <vm2615814@gmail.com>
2025-10-05 16:00:30 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,13 @@
+# question 1: write a fist python program to print hello world
+# print("hello world")
+
+# question 2: write a python program to the text exactly as a apprar
+# print("python is fun")
+# print(''''quotes and single quotes can be trickey'''')
+
+# question 3: for a bussiness create three variable to store name, age and city
+# the print a sentense that uses these variable
+name="vivek mishra"
+age=20
+city="khalilabad"
+print(f"my {name} is {age} my {city} name")
