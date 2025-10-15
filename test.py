f54f679f03d1596a02f2c6d78761e6c5541940b8
VIVEK KUMAR MISHRA <vm2615814@gmail.com>
2025-10-15 16:03:12 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,36 @@
+# # set in Python
+# my_set={1,2,4,6}
+# print(type(my_set))
+# print(my_set)
+
+# # questeion2: set operation adding elements
+# number={1,3,5,6,8}
+# number.add(13)
+# print(number)
+
+# # removing element
+# fruits={'apple','mango' ,'banana'}
+# fruits.remove('apple')
+# print(fruits)
+
+# question 3: union - combine elements from 2 sets
+# v1={1,2,4,5}
+# v2={5,4,7,8,9}
+# union_set=v1.union(v2)
+# print(union_set)
+
+# question 3: intersection combine element from 2 sets
+# v1={1,3,4,5,6}
+# v2={3,5,6,7,8,9,0}
+# inter_set=v1.intersection(v2)
+# print(inter_set)
+
+# question 5: difference element present in first set only
+# v1={1,3,4,5,6}
+# v2={3,5,6,7,8,9,0}
+# diff_set=v1.difference(v2)
+# print(diff_set)
+
+# set compreshesion
+# squars={x**3 for x in range(1,6)}
+# print(squars)
